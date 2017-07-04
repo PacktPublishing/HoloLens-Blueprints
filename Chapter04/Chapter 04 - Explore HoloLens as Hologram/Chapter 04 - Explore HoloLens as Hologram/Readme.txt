cdc5bde8cd68e4bfaada1025a849e682d8bcdaf3
Aks <akanshab@packtpub.com>
2017-07-04 18:32:49 +0530

    code files uploaded

---
+++ Chapter04/Chapter 04 - Explore HoloLens as Hologram/Chapter 04 - Explore HoloLens as Hologram/Readme.txt
@@ -0,0 +1,2 @@
+From the Unity 3D Project --> Assets, Open the Main Scene in Unity
+Then Follow the Build and Run Test for Holographic Project
---
+++ Chapter06/Chapter 06 - Remoting Monitoring Project (1)/Chapter 06 - Remoting Monitoring Project/HoloBlueprint.IoT/HoloBlueprint.Data/Floor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HoloBlueprint.Data
+{
+    public class Floor
+    {
+        public string FloorNumber { get; set; }
+        public List<Room> Rooms { get; set; }
+    }
+}
