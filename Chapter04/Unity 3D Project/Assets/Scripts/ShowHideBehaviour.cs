b8a49a78134f390b3e05fdb5a67a7ac14d9db17d
Aks <akanshab@packtpub.com>
2017-07-13 16:49:45 +0530

    files updated

---
+++ Chapter04/Unity 3D Project/Assets/Scripts/ShowHideBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Show Hide Behaviour for Speaker Details Pannel
+/// </summary>
+public class ShowHideBehaviour : MonoBehaviour
+{
+    public GameObject showHideObject;
+    public bool showhide = false;
+    private void Start()
+    {
+        try
+        {
+
+            MeshRenderer render = showHideObject.GetComponentInChildren<MeshRenderer>();
+            if (render != null)
+            {
+                render.enabled = showhide;
+            }
+        }
+        catch (System.Exception)
+        {
+        }
+
+    }
+}
+
---
+++ Chapter04/Unity 3D Project/Assets/Scripts/VoiceCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Voice Command Handler
+/// </summary>
+public class VoiceCommandHandler : MonoBehaviour
+{
+    private bool isShowing = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowSpeaker()
+    {
+        if (!isShowing)
+        {
+            var lftSpeakerDetails = GameObject.FindWithTag("speakerDetails");
+            MeshRenderer render = lftSpeakerDetails.GetComponentInChildren<MeshRenderer>();
+            gameObject.transform.Translate(0.0f, -1.0f * Time.deltaTime,
+
+            0.0f);
+
+            render.enabled = true;
+            isShowing = true;
+        }
+    }
+
+
+    public void HideSpeaker()
+    {
+        if (isShowing)
+        {
+            gameObject.transform.Translate(0.0f, 1.0f * Time.deltaTime,
+
+
+            0.0f);
+            var lftSpeakerDetails = GameObject.FindWithTag("speakerDetails");
+            MeshRenderer render = lftSpeakerDetails.GetComponentInChildren<MeshRenderer>();
+            render.enabled = false;
+            isShowing = false;
+        }
+    }
+}
+
